4001199d4aa5a9717b873d9e8996d028f0297931
DavidAD21 <davidsevillanoflores21@gmail.com>
2024-05-10 17:46:45 -0500

    Realizacion de el enlace

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from PyPDF2 import PdfReader
 from dotenv import load_dotenv
 import streamlit as st
 
+import requests
+from bs4 import BeautifulSoup
+
 # Configurar la página de Streamlit
 st.set_page_config(
     page_title="Gemini-Pro Chatbot by HJP7",
@@ -15,6 +18,13 @@ st.set_page_config(
 # Cargar variables de entorno
 load_dotenv()
 API_KEY = os.getenv('GEMINI_API_KEY')
+
+def contado_de_token(pregunta):
+    if pregunta:  
+        tokens = model.count_tokens(pregunta)
+    else:
+        tokens = 0
+    return tokens
 
 # Configurar la API de Gemini-Pro
 genai.configure(api_key=API_KEY)
@@ -32,9 +42,24 @@ def extract_text_from_pdf(file_contents):
         text += reader.pages[page_num].extract_text()
     return text
 
+# Función para extraer texto de una página web
+def extract_text_from_url(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        paragraphs = soup.find_all('p')
+        text = '\n'.join([p.get_text() for p in paragraphs])
+        return text
+    except Exception as e:
+        print("Error al extraer texto de la URL:", e)
+        return ""
+
 # Barra lateral para cargar archivo PDF
 st.sidebar.title("Gemini-Pro Configuration")
 pdf_path = st.sidebar.file_uploader("Upload a PDF file", type="pdf")
+
+# Barra lateral para ingresar enlace
+url = st.sidebar.text_input("Enter URL:")
 
 # Comprobar si se ha cargado un archivo PDF
 if pdf_path is not None:
@@ -58,8 +83,10 @@ st.markdown(
 # Sección para preguntas
 col1.markdown("Preguntas sobre el PDF o cualquier otro tema")
 question = col1.text_input("Tu: ")
-
+resu= contado_de_token(question)
+st.sidebar.markdown(f'Resultado de tokens en la pregunta:  \n {resu}')
 # Botón para enviar preguntas
+response=None
 if st.button("Enviar pregunta"):
     try:
         if pdf_text and question.strip():
@@ -73,9 +100,23 @@ if st.button("Enviar pregunta"):
     except genai.types.generation_types.StopCandidateException:
         st.error("El modelo detuvo la generación de la respuesta. Inténtalo de nuevo con otra pregunta.")
 
-# Mostrar conversación en la interfaz
+# Verificar si se proporciona un enlace y extraer texto si está presente
+if url.strip():
+    url_text = extract_text_from_url(url)
+    if url_text:
+        response = chat.send_message(instruction + url_text + question)
+        conversation_history.append(("You", "URL provided"))
+        conversation_history.append(("Bot", response.text))
+    else:
+        st.error("Couldn't extract text from the URL. Please try again with another link.")
 for sender, message in conversation_history:
     if sender == "Tú":
         col1.markdown(f"{sender}: {message}")
     elif sender == "Bot":
         col1.markdown(f"{sender}: {message}", unsafe_allow_html=True)
+    
+if response is not None:
+    resul= contado_de_token(response.text)
+    st.sidebar.markdown(f'Resultado de tokens en la respueta  \n {resul}')
+else:
+    col1.markdown('No hay datos')
